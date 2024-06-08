fb1694a877a1faf6f32252fa5633122f426b876f
monosniper <monosniper@gmail.com>
2024-06-08 06:05:24 +0500

    dasd

--- index.js
+++ index.js
@@ -32,9 +32,8 @@ app.use(express.static('uploads', {
 }));
 
 const start = () => {
-    console.log(process.env.DB_URL)
     try {
-        mongoose.connect('mongodb://root:rava2005_@mongo:27017').then(() => {
+        mongoose.connect(process.env.DB_URL).then(() => {
             app.listen(PORT, () => {
                 console.log('Server started at port ' + PORT);
             })
