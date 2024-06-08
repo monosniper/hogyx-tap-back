5ca2f2ca7477e0171e75d6fe11e170d8c3496f04
monosniper <monosniper@gmail.com>
2024-06-08 06:34:58 +0500

    cap header

--- middlewares/tg-middleware.js
+++ middlewares/tg-middleware.js
@@ -5,6 +5,7 @@ module.exports = function (req, res, next) {
 	console.log(req.get("Tg_id"))
 	console.log(req.headers.Tg_id)
 	console.log(req.headers.tg_id)
+	console.log(req.headers)
 	try {
 		const authorizationHeader = req.get("tg_id");
 		if (!authorizationHeader) {
