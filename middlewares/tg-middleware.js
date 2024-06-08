7bd0dc28e868da8aa9028f282d18950a3b3842c1
monosniper <monosniper@gmail.com>
2024-06-08 06:27:56 +0500

    cap header

--- middlewares/tg-middleware.js
+++ middlewares/tg-middleware.js
@@ -1,11 +1,10 @@
 const ApiError = require('../exceptions/api-error');
 
 module.exports = function (req, res, next) {
-	console.log(req.headers)
-	console.log(req.headers.Tg_id)
+	console.log(req.get("tg_id"))
 	console.log(req.headers.tg_id)
 	try {
-		const authorizationHeader = req.headers.Tg_id;
+		const authorizationHeader = req.get("tg_id");
 		if (!authorizationHeader) {
 			return next(ApiError.UnauthorizedError());
 		}
