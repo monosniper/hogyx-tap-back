2a2a323f56ee363cfc1e7414fea94c9c9972181a
Замалдинов Равиль <vanteateam@gmail.com>
2024-06-27 19:44:05 +0500

    upd

--- config/levels.js
+++ config/levels.js
@@ -1,49 +1,49 @@
 module.exports = {
-    1: {
-	value: 0,
-    },
-    2: {
-	value: 10000,
-	add_energy: 125,
-    },
-    3: {
-	value: 20000,
-	add_energy: 125,
-    },
-    4: {
-	value: 50000,
-	add_energy: 125,
-    },
-    5: {
-	value: 100000,
-	add_energy: 125,
-    },
-    6: {
-	value: 200000,
-	add_energy: 250,
-    },
-    7: {
-	value: 500000,
-	add_energy: 250,
-    },
-    8: {
-	value: 1000000,
-	add_energy: 250,
-    },
-    9: {
-	value: 2000000,
-	add_energy: 250,
-    },
-    10: {
-	value: 5000000,
-	add_energy: 500,
-    },
-    11: {
-	value: 10000000,
-	add_energy: 500,
-    },
-    12: {
-	value: 50000000,
-	add_energy: 1000,
-    },
+	1: {
+		value: 0,
+	},
+	2: {
+		value: 10000,
+		add_energy: 125,
+	},
+	3: {
+		value: 20000,
+		add_energy: 125,
+	},
+	4: {
+		value: 50000,
+		add_energy: 125,
+	},
+	5: {
+		value: 100000,
+		add_energy: 125,
+	},
+	6: {
+		value: 200000,
+		add_energy: 250,
+	},
+	7: {
+		value: 500000,
+		add_energy: 250,
+	},
+	8: {
+		value: 1000000,
+		add_energy: 250,
+	},
+	9: {
+		value: 2000000,
+		add_energy: 250,
+	},
+	10: {
+		value: 5000000,
+		add_energy: 500,
+	},
+	11: {
+		value: 10000000,
+		add_energy: 500,
+	},
+	12: {
+		value: 50000000,
+		add_energy: 1000,
+	},
 }
