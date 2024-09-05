bfb0c81701a54f682980fed234dc73dfe334fd75
ravilto <vanteateam@gmail.com>
2024-09-05 14:48:03 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 const TelegramBot = require("node-telegram-bot-api");
 
 const bot = new TelegramBot(process.env.BOT_TOKEN, {
-    testEnvironment: true
+    // testEnvironment: true,
+    testEnvironment: false,
 });
 
 module.exports = bot
